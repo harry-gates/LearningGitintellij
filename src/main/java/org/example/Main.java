89aae2ef75c7cf3584c73fade9e59c09984e9bf8
HariomGupta <hariomgupta9286@gmail.com>
2025-09-23 15:05:43 +0530

    Added new line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("New test");
+        System.out.println("New line to commit");
     }
 }
